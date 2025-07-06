608fa24f97ae20e8e35b155fbaafd365668e080c
WynandColemanKapsch <wynand.coleman@kapsch.net>
2025-07-06 20:43:11 +0100

    fixed security group id for rules

--- main.tf
+++ main.tf
@@ -42,6 +42,8 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_all_out" {
@@ -50,4 +52,6 @@ resource "aws_security_group_rule" "blog_all_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
