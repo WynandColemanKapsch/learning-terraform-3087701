dfbf4fd8e3e152c2ac3c65e009bb7e26df16284b
WynandColemanKapsch <wynand.coleman@kapsch.net>
2025-07-08 00:07:46 +0100

    test scalling

--- main.tf
+++ main.tf
@@ -48,14 +48,14 @@ resource "aws_instance" "blog" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
   
   name ="blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  autoscaling_group_target_group_arns = module.blog_alb.target_group_arns
+  target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id = data.aws_ami.app_ami.id
