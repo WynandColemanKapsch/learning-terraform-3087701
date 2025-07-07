90c453de7c8b45e902271bcb13b430c24c16949c
WynandColemanKapsch <wynand.coleman@kapsch.net>
2025-07-07 22:26:48 +0100

    test lb

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ module "alb" {
 
   name    = "blog-"
   vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.subnets
-  security_groups = [module.blog_vpc.security_group_id]
+  subnets = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = [
     {
