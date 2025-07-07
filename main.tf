bd29caea7d346ca73273a7cc01b47b60f8b8ff72
WynandColemanKapsch <wynand.coleman@kapsch.net>
2025-07-07 23:56:45 +0100

    scalling

--- main.tf
+++ main.tf
@@ -46,7 +46,23 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  
+  name ="blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id = data.aws_ami.app_ami.id
+  instance = var.instance_type
+}
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "4.0.0"
 
@@ -61,8 +77,7 @@ module "alb" {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
-      target_type      = "instance"
-      target_id = aws_instance.blog.id        
+      target_type      = "instance"    
     }
   ]
 
